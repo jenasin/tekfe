258e20b1a960c3f63f406f33b70457c42fe8624c
Jan Saro <sarojanek@gmail.com>
2022-09-01 09:01:39 +0200

    modified workflow file and installed Axios

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // Test api call
+  const axios = require('axios').default;
+  // Make a request for a user with a given ID
+  axios.get('https://stepa.azurewebsites.net/api/WeatherForecast')
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+
   return (
     <div className="App">
       <header className="App-header">
